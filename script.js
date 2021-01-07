da5a2c681395f8468914b2bdf23e55d8d1562e91
mabadinakach <mabadinakach@gmail.com>
2021-01-07 11:25:54 -0600

    init google calendar API

--- script.js
+++ script.js
@@ -180,3 +180,46 @@ all_items_button.forEach(elt => elt.addEventListener('click', (index) => {
     console.log(data[elt.getAttribute('id')])
     window.open(data[elt.getAttribute('id')]["link"])
 }))
+
+var CLIENT_ID = '486906308520-7gapsff55hia8hj8kvgk3mdctjojtilo.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyChNbkCvbk2KBayw2nzgaz-bW5BveZOsO4';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+*  On load, called to load the auth2 library and API client library.
+*/
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+*  Initializes the API client library and sets up sign-in state
+*  listeners.
+*/
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function(error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
